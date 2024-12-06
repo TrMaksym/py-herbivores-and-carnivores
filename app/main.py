e56f0ac0c8eb49e63d0a61d77a1eb423f7e9f7b6
Maksym <maximuschampion2002@gmail.com>
2024-12-06 22:34:30 +0200

    herbivores-and-carnivores

--- app/main.py
+++ app/main.py
@@ -1,1 +1,51 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def take_damage(self, damage: int) -> None:
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+        print(f"{self.name} took {damage} damage. Health: {self.health}")
+
+        if self.health == 0:
+            self.die()
+
+    def die(self) -> None:
+        if self in Animal.alive:
+            Animal.alive.remove(self)
+        print(f"{self.name} is dead.")
+
+    def is_alive(self) -> bool:
+        return self.health > 0
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}"
+                )
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+        print(f"{self.name} is now {"hidden" if self.hidden else "visible"}.")
+
+
+class Carnivore(Animal):
+    def bite(self, herbivore: Herbivore) -> None:
+        if isinstance(herbivore, Herbivore):
+            if herbivore.hidden:
+                print(f"{self.name} cannot bite {herbivore.name} "
+                      f"because they are hidden.")
+            else:
+                herbivore.take_damage(50)
+                print(f"{self.name} bit {herbivore.name}. "
+                      f"{herbivore.name}'s health: {herbivore.health}")
+        else:
+            print(f"{self.name} cannot bite another carnivore.")
